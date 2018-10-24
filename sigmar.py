e6e883371c0a81978d0efedd9020c8542e1a267d
James Bartlett <jimbobarto@yahoo.co.uk>
2018-10-24 22:12:49 +0100

    Oops - get rid of the print statements

--- sigmar.py
+++ sigmar.py
@@ -12,10 +12,8 @@ def init():
 @app.route('/_get_filtered_cards')
 def get_filtered_cards():
 	search_string = request.args.get('search_string')
-	print(search_string)
 	prefixed_cards = [filename for filename in listdir('static/images/library/') if (filename.find(search_string) > -1 and not (filename.startswith('.')))]
 	prefixed_cards.sort()
-	print(prefixed_cards)
 	return render_template('card-list.html', cards=prefixed_cards)
 
 @app.route('/_get_all_cards')
@@ -23,7 +21,6 @@ def get_all_cards():
 	return render_template('card-list.html', cards=get_all_cards())
 
 def get_all_cards():
-	print("In get all cards")
 	cards = [filename for filename in listdir('static/images/library/') if (isfile(join('static/images/library/', filename)) and not (filename.startswith('.')))]
 	cards.sort()
 	return cards
