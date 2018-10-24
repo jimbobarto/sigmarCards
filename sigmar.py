5f0895de130666788a173ad48ac6ac6bb319db72
James Bartlett <jimbobarto@yahoo.co.uk>
2018-10-24 21:53:59 +0100

    search on type in search box, plus some refactors

--- sigmar.py
+++ sigmar.py
@@ -6,20 +6,25 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-	cards = [f for f in listdir('static/images/library/') if isfile(join('static/images/library/', f))]
-	cards.sort()
-	return render_template('drag.html', cards=cards)
+def init():
+	return render_template('drag.html', cards=get_all_cards())
 
-@app.route('/_get_cards')
-def get_cards():
+@app.route('/_get_filtered_cards')
+def get_filtered_cards():
 	search_string = request.args.get('search_string')
-	prefixed_cards = [filename for filename in listdir('static/images/library/') if filename.startswith(search_string)]
+	print(search_string)
+	prefixed_cards = [filename for filename in listdir('static/images/library/') if (filename.find(search_string) > -1 and not (filename.startswith('.')))]
 	prefixed_cards.sort()
-	return jsonify(prefixed_cards)
+	print(prefixed_cards)
+	return render_template('card-list.html', cards=prefixed_cards)
 
 @app.route('/_get_all_cards')
 def get_all_cards():
-	cards = [f for f in listdir('static/images/library/') if isfile(join('static/images/library/', f))]
+	return render_template('card-list.html', cards=get_all_cards())
+
+def get_all_cards():
+	print("In get all cards")
+	cards = [filename for filename in listdir('static/images/library/') if (isfile(join('static/images/library/', filename)) and not (filename.startswith('.')))]
 	cards.sort()
-	return render_template('card-list.html', cards=cards)
+	return cards
+
---
+++ static/js/sigmar.js
@@ -0,0 +1,99 @@
+$( setDraggable );
+
+function setDraggable() {
+	$(".draggable").draggable({
+	  revert: 'invalid',
+	  appendTo: 'body',
+	  helper: 'clone'
+	});
+	$( ".droppable" ).droppable({
+		classes: {
+				'ui-droppable-hover': 'highlight'
+		},
+      drop: function( event, ui ) {
+        var $this = $(this);
+        //ui.draggable.removeClass("draggable-card");
+        ui.draggable.addClass("hidden");
+        hideDroppedCardsInList();
+
+        addImage(ui.draggable.find("img.card").attr("src"), $this);
+        
+	    ui.draggable.position({
+	      my: "center",
+	      at: "center",
+	      of: $this,
+	      using: function(pos) {
+	        $(this).animate(pos, 10, "linear");
+	      }
+	    });
+      }
+    });
+}
+
+function hideDroppedCardsInList() {
+	$("div.droppable img").each( function(index) {
+		var droppedSource = $(this).attr("src");
+		if ($(".draggable img[src='" + droppedSource + "']").length) {
+			$(".draggable img[src='" + droppedSource + "']").closest(".draggable").addClass("hidden");
+		}
+	})
+}
+
+function screenshot() {
+	html2canvas(document.body, {taintTest: false, allowTaint: true}).then(canvas => {
+			//var image = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");  // here is the most important part because if you dont replace you will get a DOM 18 exception.
+			canvas.toBlob(function(blob){
+				var a = document.createElement('a');
+				a.href = URL.createObjectURL(blob);
+ 			a.download = 'screenshot.png';
+				if ($("#screenshot-name").val() != "") {
+					a.download = $("#screenshot-name").val();	
+				}
+ 			a.click();
+		});
+	});
+}
+
+function revert(currentElement) {
+	var source = $(currentElement).closest(".droppable").find("img.resize").attr('src');
+	$(".draggable img[src='" + source + "']").siblings("span.card-name").removeClass("hidden");
+	$(".draggable img[src='" + source + "']").closest(".draggable").attr("style", "position: relative;");
+
+	$(".draggable img[src='" + source + "']").closest(".draggable").addClass("draggable-card");
+
+	$(currentElement).closest(".droppable").find("div.card-holder").remove();
+}
+
+function clearAllCards() {
+	$("div#card-list .draggable").each( function(index) {
+			$(this).removeClass("hidden");
+			$(this).attr("style", "position: relative;");
+			$(this).addClass("draggable-card");
+  	});
+	$(".droppable").each( function(index) {
+			$(this).find("div.card-holder").remove();
+  	});
+}
+
+function addImage(imageSource, destination) {
+	destination.html("<div class='card-holder'><img class='resize' src='" + imageSource + "'/><div class='top-right'><a onclick='revert(this);'><img src='" + "{{url_for('static', filename='images/cancel.png')}}" + "'/></a></div></div>");
+}
+
+function cardSearch() {
+	$.get($SCRIPT_ROOT + '/_get_filtered_cards', {
+        search_string: $('input#card_search').val()
+      }, function(data) {
+      	$("div#card-list").html(data);
+      	setDraggable();
+      	hideDroppedCardsInList();
+  	});
+}
+
+function clearCardSearch() {
+	$.get($SCRIPT_ROOT + '/_get_all_cards', function(data) {
+      	$("div#card-list").html(data);
+      	setDraggable();
+      	hideDroppedCardsInList();
+  	});
+	$("#card_search").val("");
+}
